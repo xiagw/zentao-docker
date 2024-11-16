c82b2cf9198c3b24f88f8a5d6fb291025644dcc7
xiagw <fxiaxiaoyu@gmail.com>
2024-11-16 22:21:47 +0800

    fix: public-build.sh let code more DRY

--- hack/make-rules/public-build.sh
+++ hack/make-rules/public-build.sh
@@ -10,34 +10,24 @@ dockerfile=${6:-Dockerfile}
 buildEnv=${7:-internal}
 buildDate=$(date +%Y%m%d)
 
-
 publicRepository="hub.zentao.net/app/$appName"
 baseImage="hub.zentao.net/ci/zentao-runtime:php${phpVer%.*}-mysql${mysqlVer%.*}"
 
-if [[ $appVer =~ ^[0-9]+.[0-9]+ ]];then
-    docker buildx build \
-                --build-arg BASE_IMAGE="$baseImage" \
-                --build-arg ZENTAO_VER="$appVer" \
-                --build-arg ZENTAO_URL="$ZENTAO_URL" \
-                --build-arg PHP_VER="$phpVer" \
-                --build-arg MYSQL_VER="$mysqlVer" \
-                --build-arg BUILD_ENV="$buildEnv" \
-                --platform="$arch" \
-                -t $publicRepository:$appVer-$buildDate \
-                -t $publicRepository:$appVer \
-                -t $publicRepository \
-                -f "$dockerfile" . --pull --push
-else
+# 构建基本的标签列表
+tags=(-t "$publicRepository:$appVer-$buildDate" -t "$publicRepository:$appVer")
 
-    docker buildx build \
-                --build-arg BASE_IMAGE="$baseImage" \
-                --build-arg ZENTAO_VER="$appVer" \
-                --build-arg ZENTAO_URL="$ZENTAO_URL" \
-                --build-arg PHP_VER="$phpVer" \
-                --build-arg MYSQL_VER="$mysqlVer" \
-                --build-arg BUILD_ENV="$buildEnv" \
-                --platform="$arch" \
-                -t $publicRepository:$appVer-$buildDate \
-                -t $publicRepository:$appVer \
-                -f "$dockerfile" . --pull --push
+# 如果版本号符合 x.y 格式，添加 latest 标签
+if [[ $appVer =~ ^[0-9]+\.[0-9]+ ]]; then
+    tags+=(-t "$publicRepository")
 fi
+
+docker buildx build \
+    --build-arg BASE_IMAGE="$baseImage" \
+    --build-arg ZENTAO_VER="$appVer" \
+    --build-arg ZENTAO_URL="$ZENTAO_URL" \
+    --build-arg PHP_VER="$phpVer" \
+    --build-arg MYSQL_VER="$mysqlVer" \
+    --build-arg BUILD_ENV="$buildEnv" \
+    --platform="$arch" \
+    "${tags[@]}" \
+    -f "$dockerfile" . --pull --push
